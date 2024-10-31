425999e6f2d486f8e61e6a12ec3882e7ae36d881
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-10-30 21:55:35 -0500

    schema update

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLSchema, GraphQLID, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLSchema, GraphQLID, GraphQLString, GraphQLInt, GraphQLList, GraphQLNonNull } = graphql;
 
 // Dummy data for tasks
 const tasks = [
@@ -8,13 +8,15 @@ const tasks = [
     id: '1',
     title: 'Create your first webpage',
     weight: 1,
-    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+    description: 'Create your first HTML file 0-index.html...',
+    projectId: '1'
   },
   {
     id: '2',
     title: 'Structure your webpage',
     weight: 1,
-    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
+    description: 'Copy the content of 0-index.html into 1-index.html...',
+    projectId: '1'
   }
 ];
 
@@ -24,13 +26,13 @@ const projects = [
     id: '1',
     title: 'Advanced HTML',
     weight: 1,
-    description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'
+    description: 'Welcome to the Web Stack specialization...',
   },
   {
     id: '2',
     title: 'Bootstrap',
     weight: 1,
-    description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'
+    description: 'Bootstrap is a free and open-source CSS framework...',
   }
 ];
 
@@ -41,7 +43,13 @@ const TaskType = new GraphQLObjectType({
     id: { type: GraphQLID },
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
-    description: { type: GraphQLString }
+    description: { type: GraphQLString },
+    project: {
+      type: ProjectType,
+      resolve(parent, args) {
+        return _.find(projects, { id: parent.projectId });
+      }
+    }
   })
 });
 
@@ -52,7 +60,13 @@ const ProjectType = new GraphQLObjectType({
     id: { type: GraphQLID },
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
-    description: { type: GraphQLString }
+    description: { type: GraphQLString },
+    tasks: {
+      type: new GraphQLList(TaskType),
+      resolve(parent, args) {
+        return _.filter(tasks, { projectId: parent.id });
+      }
+    }
   })
 });
 
@@ -64,7 +78,6 @@ const RootQuery = new GraphQLObjectType({
       type: TaskType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // Find the task by id using lodash
         return _.find(tasks, { id: args.id });
       }
     },
@@ -72,7 +85,6 @@ const RootQuery = new GraphQLObjectType({
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // Find the project by id using lodash
         return _.find(projects, { id: args.id });
       }
     }
