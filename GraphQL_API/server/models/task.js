cbca5b6e4379abaf9172618bf4b6fe1cf42fff95
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-10-31 01:58:51 -0500

    git ignore

--- GraphQL_API/server/models/task.js
+++ GraphQL_API/server/models/task.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const taskSchema = new mongoose.Schema({
+const taskSchema = new Schema({
   title: { type: String, required: true },
   weight: { type: Number, required: true },
   description: { type: String, required: true },
-  projectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
-  // Add other fields if necessary
+  projectId: { type: Schema.Types.ObjectId, ref: 'Project', required: true }
 });
 
 module.exports = mongoose.model('Task', taskSchema);
