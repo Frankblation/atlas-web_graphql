5a87ff8f91504194dbf9b8631e4f224f7f52eb5f
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-10-31 01:14:01 -0500

    database connected

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+// Use your updated connection string here
+const uri = 'mongodb+srv://frankblation:SpSyKfYRMX4bsSjm@atlascluster.dkfih.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 const app = express();
 
--- GraphQL_API/server/models/task.js
+++ GraphQL_API/server/models/task.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    weight: { type: Number, required: true },
-    description: { type: String, required: true },
-    projectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true }
+  title: { type: String, required: true },
+  weight: { type: Number, required: true },
+  description: { type: String, required: true },
+  projectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
+  // Add other fields if necessary
 });
 
 module.exports = mongoose.model('Task', taskSchema);
